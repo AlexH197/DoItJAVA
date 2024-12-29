726cf52c5c704cc39f7a51e2072280c1e658f23e
AlexH197 <geomju777@gmail.com>
2024-12-29 12:19:01 +0900

    third commit.

---
+++ src/chapter2/ImplicitConversion.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class ImplicitConversion {
+    public static void main(String[] args) {
+        byte bNUM = 10;
+        int iNUM = bNUM;
+
+        System.out.println(bNUM);
+        System.out.println(iNUM);
+
+        int iNUM2 = 20;
+        float fNUM = iNUM2;
+
+        System.out.println(iNUM);
+        System.out.println(fNUM);
+
+        double dNUM;
+        dNUM = fNUM + iNUM;
+        System.out.println(dNUM);
+    }
+}
